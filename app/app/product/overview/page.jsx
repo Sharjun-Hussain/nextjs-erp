5633ee275268827677e1298c91d695b3e9e6f2ad
Sharjun Hussain <sharjoonibnuhussain99@gmail.com>
2025-01-14 10:28:01 +0530

    fix:fixed layout issue

--- app/app/product/overview/page.jsx
+++ app/app/product/overview/page.jsx
@@ -6,14 +6,6 @@ import React, { useEffect, useState } from "react";
 
 const page = () => {
   const availableWidth = useAvailableWidth();
-  const [windowWidth, setwindowWidth] = useState(availableWidth);
-
-  //   useEffect(() => {
-  //     console.log("Sidebar state:", state);
-  //     console.log("Calculated available width:", screenwidth);
-  //     console.log("sidebar width:", sidebar_width);
-  //     console.log("toggled sidebar width width:", sidebar_toggled_width);
-  //   }, [state]);
 
   return (
     <div>
